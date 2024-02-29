cc34412b4570b199e884158e155094c36b2a3185
6apsuk <161694543+6apsuk@users.noreply.github.com>
2024-02-29 19:47:51 +0500

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,6 +1,5 @@
 import re
 import random
-
 def check_symbols (value):
     symbols_pattern = r"[<>, *&^%$#@! -=+'0-9]"
     symbols_found = re.findall (symbols_pattern, value)
@@ -8,23 +7,18 @@ def check_symbols (value):
         return True 
     else:
         return False
-
 def generate_password (value) :
     letters = '+-/*!&$#?=@<>abcdefghiiklnopgrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    value = ‘’
+    value = ''
     for i in range (10):
         value += random. choice (letters)
     return value
-        value += random. choice(letters)
-    return value
-
 def validate_password(value):
     password_length = len(value)
-    if password_length › 8 and check_symbols(value):
+    if password_length > 8 and check_symbols(value):
         print("Your password is quite good! You can use it.")
     else:
         good_pass = generate_password (value)
         print(f"Your password is not good enough... May be try this one: {good_pass}")
 value = input( 'Enter a password to check: ')
-vlidate_password (value)
-
+validate_password(value)
